47bd2818ec66e9666a0c3137ed9a567502222a3f
LucasAlberola <lap54@alu.ua.es>
2024-12-01 21:16:07 +0100

    Added GUI and removed key listening on EC_S.py

--- EC_S.py
+++ EC_S.py
@@ -3,6 +3,8 @@ import sys
 import time
 from pynput import keyboard
 import threading
+import tkinter as tk
+from tkinter import ttk
 
 global stop, connected
 FORMAT = "utf-8"
@@ -10,20 +12,23 @@ HEADER = 1024
 OK = "OK".encode(FORMAT)
 KO = "KO".encode(FORMAT)
 
-# DESCRIPTION: Envía el estado del sensor, según el valor de la variable stop
+# DESCRIPTION: Envía de forma contínua el estado del sensor, según el valor de la variable stop
 # STARTING_VALUES: NONE
 # RETURNS: NONE
 # NEEDS: NONE
 def sendState():
-    global connected
+    global connected, stop
     try:
-        if connected:
+        while True:
+            # if connected:
             # Send OK
             if not stop:
                 client.send(OK)
             # Send KO
             else:
                 client.send(KO)
+            time.sleep(1)
+            print(f"Sent: {stop}")
     except Exception as e:
         print(f"[MESSAGE SENDER] SOMETHING WENT WRONG: {e}")
         print("[SENSOR APPLICATION] STOPPING SENSOR SERVICE...")
@@ -47,64 +52,106 @@ def receive():
         connected = False
         client.close()  
 
-# DESCRIPTION: Cambia el valor de stop a True
-# STARTING_VALUES: NONE
+# DESCRIPTION: Cambia el estado del sensor
+# STARTING_VALUES: el marco principal de la aplicación
 # RETURNS: NONE
 # NEEDS: NONE
-def keyPressed(tecla):
+def changeState(button, stateLabel):
     global stop
-    stop = True
-    print("[KEYBOARD LISTENER] Key was pressed")
-
-# DESCRIPTION: Cambia el valor de stop a False
-# STARTING_VALUES: NONE
-# RETURNS: NONE
-# NEEDS: NONE
-def keyReleased(tecla):
-    print("[KEYBOARD LISTENER] Key was released")
-    global stop
-    stop = False
-
-# DESCRIPTION: Inicia la detección de pulsación de teclas
-# STARTING_VALUES: NONE
-# RETURNS: NONE
-# NEEDS: NONE
-def startListener():
-    try:
-        with keyboard.Listener(on_press=keyPressed, on_release=keyReleased) as listener:
-            listener.join()
-    # Manage any exception ocurred
-    except Exception as e:
-        print(f"[KEYBOARD LISTENER] AN ERROR OCURRED WHILE SENDING MY STATE: {e}")
-        listener.close()
+    stop = not stop
+    currentState = "OK"
+    buttonText = "Parar"
         
+    if stop or not connected:
+        currentState = "KO"
+        buttonText = "Iniciar"
+
+
+    button.config(text=buttonText)
+    stateLabel.config(text=currentState)
+
+# DESCRIPTION: Actualiza los elementos de la interfaz gráfica
+# STARTING_VALUES: el marco principal de la aplicación
+# RETURNS: NONE
+# NEEDS: NONE
+def updateGUI(root, button, stateLabel):
+    #For updating the GUI in the future
+        
+    root.after(1000, lambda: updateGUI(root, button, stateLabel))
+      
+# DESCRIPTION: Crea los elementos de la interfaz gráfica
+# STARTING_VALUES: el marco principal de la aplicación
+# RETURNS: NONE
+# NEEDS: NONE
+def designInterface(mainFrame):
+    global stop, connected
+    buttonFrame = ttk.Frame(mainFrame)
+    buttonFrame.grid(row=1, column=0, padx=10, pady=10)
+
+    buttonLabel = ttk.LabelFrame(buttonFrame, text="ESTADO DEL SENSOR "+ str(ID))
+    currentState="OK"
+    if stop or not connected:
+        currentState = "KO"
+    connectionLabel = ttk.Label(buttonLabel, text="Conectado a "+ str(SERVER) + ":" +  str(PORT))
+    stateLabel = ttk.Label(buttonLabel, text=currentState)
+    buttonLabel.pack(padx=5, pady=5)
+    connectionLabel.pack(padx=5, pady=5)
+    stateLabel.pack(padx=5, pady=5)
+    
+
+    buttonText = "Iniciar" if stop else "Parar"
+    button = ttk.Button(buttonLabel, text=buttonText, command=lambda: changeState(button, stateLabel))
+    button.pack(side=tk.BOTTOM, padx=5)
+
+    return button, stateLabel
+
+
 
 #############################
 #   Program starting point  #
 #############################
-if  (len(sys.argv) == 3):
+if  (len(sys.argv) == 4):
     SERVER = sys.argv[1]
     PORT = int(sys.argv[2])
+    ID = str(sys.argv[3])
     ADDR = (SERVER, PORT)
     stop = False
     connected = True
     try:
         # Begin the listener thread
-        listenerThread = threading.Thread(target=startListener)
-        listenerThread.daemon = True
-        listenerThread.start()
+        # listenerThread = threading.Thread(target=startListener)
+        # listenerThread.daemon = True
+        # listenerThread.start()
 
-        #Open up connection
+        # Open up connection
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client.connect(ADDR)
-
+        
+        # Start to process received data
         thread = threading.Thread(target=receive)
         thread.daemon = True
         thread.start()
     
-        while listenerThread.is_alive and connected:
-            time.sleep(1)
-            sendState()
+        # Starting thread for sending state
+        thread = threading.Thread(target=sendState)
+        thread.daemon = True
+        thread.start()
+        # while listenerThread.is_alive and connected:
+        #     time.sleep(1)
+        #     sendState()
+
+        # Graphical User Interface
+        root = tk.Tk()
+        root.title("EasyCab Central Control Panel")
+        mainFrame = ttk.Frame(root, padding="10")
+        mainFrame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
+        # Create tables for taxi and customer
+        button = designInterface(mainFrame)
+
+
+        # root.after(1000, lambda: updateGUI(root, button))
+
+        root.mainloop()
 
     except ConnectionError:
         print(f"Unable to find DIGITAL ENGINE on {ADDR}")
@@ -119,4 +166,4 @@ if  (len(sys.argv) == 3):
         print(f'EXITING SENSOR APPLICATION')
 
 else:
-    print("SORRY, INCORRECT PARAMETER USE. \nUSAGE: <DIGITAL ENGINE IP> <PORT>")
+    print("SORRY, INCORRECT PARAMETER USE. \nUSAGE: <DIGITAL ENGINE IP> <PORT> <SENSOR ID>")
