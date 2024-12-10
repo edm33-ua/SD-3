a3aa9b3cee57ade1ed5c6d785fe1a716210d2c5e
Emiliano <edm33@alu.ua.es>
2024-12-10 22:10:48 +0100

    EC_CTC and EC_Registry prototypes. As well as the EC_Registry certificate and the file with the OpenWeatherKey

--- EC_CTC.py
+++ EC_CTC.py
@@ -1,14 +1,127 @@
-# import required modules
 from flask import Flask, request, jsonify
 import requests
 import threading
+import tkinter as tk
 
+# El documento con la KEY para usar OpenWeather
 weatherKeyFile = "OpenWeatherKey.txt"
 # La URL base para la conexión de OpenWeather
 BASE_API_URL = "http://api.openweathermap.org/data/2.5/weather?"
 
 # Definimos las variables globales
-global CITY, CITYLock
+City = 'Madrid'
+Temp = '-'
+################  CHANGE CITY  #################
+
+# DESCRIPTION: Este método se encarga de actualizar el valor de la ciudad con el de la caja de texto del menú, y la temperatura por la
+# correspondiente de su ciudad
+# STARTING_VALUES: NONE
+# RETURNS: NONE
+# NEEDS: NONE
+def updateCity():
+    try:
+        # We define the global variables
+        global textbox, cityLabel, City, Temp, tempLabel
+        # Then we assign the value and delete the residue from the textbox
+        City = textbox.get()
+        textbox.delete(first=0, last="end")
+        # We get the new temperature
+        callOpenWeather()
+        cityLabel.config(text=f"Ciudad seleccionada: {City}")
+        tempLabel.config(text=f"Temperatura: {round(Temp,2)}")
+    except Exception as e:
+        print(f'[MENU HANDLER] THERE HAS BEEN AN ERROR WHILE UPDATING THE CITY. {e}')
+
+# DESCRIPTION: Este método se encarga de generar la ventana con el menú para cambiar el sistema
+# STARTING_VALUES: NONE
+# RETURNS: NONE
+# NEEDS: updateCity()
+def windowMenu():
+    try:
+        global textbox, cityLabel, tempLabel
+        # We create the main window
+        window = tk.Tk()
+        window.title("Selector de Ciudad")
+        window.geometry("600x400")
+        window.config(padx=20, pady=60)
+
+        # We create the label to show the selected city
+        cityLabel = tk.Label(window, text="Ciudad seleccionada: Ninguna", font=("Times New Roman", 16))
+        cityLabel.pack(pady=10)
+
+        # We create the label to show the last asked temperature
+        tempLabel = tk.Label(window, text=f"Temperatura: -", font=('Times New Roman', 16))
+        tempLabel.pack(pady=10)
+
+        # We create the textbox where we'll introduce the new city
+        textbox = tk.Entry(window, font=("Times New Roman Greek", 12), width=20)
+        textbox.pack(pady=10)
+
+        # We create the button to upload the city value
+        button = tk.Button(window, text="Actualizar Ciudad", command=updateCity, font=("Times New Roman Baltic", 14))
+        button.pack(pady=10)
+
+        # We begin the loop with the main window
+        window.mainloop()
+    except KeyboardInterrupt:
+        pass
+    except Exception as e:
+        print(f'[MENU HANDLER] THERE HAS BEEN AN ERROR WHILE CREATING THE MENU. {e}')
+
+# DESCRIPTION: Este método se encarga de generar el menú para cambiar la ciudad del sistema y facilitar dicho proceso
+# STARTING_VALUES: NONE
+# RETURNS: NONE
+# NEEDS: windowMenu()
+def changeCitySystem():
+    try:
+        windowMenu()
+    except KeyboardInterrupt:
+        print(f"[CITY MANAGER] APPLICATION ENDED DUE TO HUMAN INTERACTION")
+    except Exception as e:
+        print(f"[CITY MANAGER] THERE HAS BEEN AN ERROR WHILE MANTAINING THE CITY. {e}")
+
+###############  OPENWEATHER OPERATIONS  ###############
+
+# DESCRIPTION: Este método recupera la Key para la API de OpenWeather desde un archivo
+# STARTING_VALUES: NONE
+# RETURNS: La key en string
+# NEEDS: NONE
+def getOpenWeatherKey():
+    # Declaramos las variables globales
+    global weatherKeyFile
+    try:
+        # Abrimos el archivo, leemos la key, y lo cerramos
+        file = open(weatherKeyFile)
+        key = file.readline()
+        file.close()
+        return key
+    except Exception as e:
+        print(f"[OPENWEATHER KEY] THERE HAS BEEN AN ERROR WHILE READING THE KEY. {e}")
+
+# DESCRIPTION: Este método realiza una llamada a OpenWeather con la ciudad introducida
+# STARTING_VALUES: NONE
+# RETURNS: OK o KO
+# NEEDS: getOpenWeatherKey()
+def callOpenWeather():
+	# Declaramos las variables globales
+    global City, BASE_API_URL, Temp
+    try:
+        requestURL = BASE_API_URL + "appid=" + getOpenWeatherKey() + "&q=" + City
+        response = requests.get(requestURL)
+        jsonResponse = response.json()
+        if jsonResponse["cod"] != '404':
+            x = jsonResponse["main"]
+            cityTemperature = x["temp"] - 273.15
+            Temp = cityTemperature
+            if cityTemperature > 0:
+                return 'OK'
+            else:
+                return 'KO'
+        else:
+            print("[OPENWEATHER CALLER] The selected city is invalid")
+            return 'ERR'
+    except Exception as e:
+        print(f'[OPENWEATHER CALLER] THERE HAS BEEN AN ERROR WHILE CALLING THE OPENWEATHER API. {e}')
 
 ################  API DEFINITION  ################
 
@@ -18,85 +131,25 @@ app = Flask(__name__)
 # STARTING_VALUES: NONE
 # RETURNS: La respuesta en versión JSON
 # NEEDS: NONE
-@app.route('/getWeather', methods=['POST'])
+@app.route('/getWeather', methods=['GET'])
 def getWeather():
+    global City
     response = callOpenWeather()
     return jsonify({"response": response})  # Respuesta en JSON
 
-# DESCRIPTION: Este método actúa como wrapper para la llamada de API
-# STARTING_VALUES: NONE
-# RETURNS: NONE
-# NEEDS: NONE
-def apiWrapper():
-    app.run(debug=True, port=5000)
-
-# DESCRIPTION: Este método recupera la Key para la API de OpenWeather desde un archivo
-# STARTING_VALUES: NONE
-# RETURNS: La key en string
-# NEEDS: NONE
-def getOpenWeatherKey():
-
-    try:
-        # Abrimos el archivo, leemos la key, y lo cerramos
-        file = open(weatherKeyFile)
-        key = file.readline()
-        file.close()
-        
-        return key
-    except Exception as e:
-        print(f"[OPENWEATHER KEY] THERE HAS BEEN AN ERROR WHILE READING THE KEY. {e}")
-
-# DESCRIPTION: Este método realiza una llamada a OpenWeather con la ciudad introducida
-# STARTING_VALUES: NONE
-# RETURNS: OK o KO
-# NEEDS: getOpenWeatherKey()
-def callOpenWeather(CITY):
-	
-    try:
-        requestURL = BASE_API_URL + "appid=" + getOpenWeatherKey() + "&q=" + CITY
-        response = requests.get(requestURL)
-        jsonResponse = response.json()
-        if jsonResponse["cod"] != '404':
-            x = jsonResponse["main"]
-            cityTemperature = x["temp"] - 273.15
-            if cityTemperature > 0:
-                return 'OK'
-            else:
-                return 'KO'
-        else:
-            print("[OPENWEATHER CALLER] The selected city is invalid")
-    except Exception as e:
-        print(f'[OPENWEATHER CALLER] THERE HAS BEEN AN ERROR WHILE CALLING THE OPENWEATHER API. {e}')
-
-# DESCRIPTION: Este método cambia la ciudad del sistema
-# STARTING_VALUES: NONE
-# RETURNS: NONE
-# NEEDS: NONE
-def changeCitySystem():
-
-    try:
-        # Declaramos las variables globales
-        global CITY, CITYLock
-
-        city = input("Introduzca la ciudad a la que quiere cambiar: ")
-        CITYLock.acquire()
-        CITY = city
-        CITYLock.release()
-    except KeyboardInterrupt:
-        print(f"[CITY MANAGER] APPLICATION ENDED DUE TO HUMAN INTERACTION")
-    except Exception as e:
-        print(f"[CITY MANAGER] THERE HAS BEEN AN ERROR WHILE CHANGING THE CITY. {e}")
-
 #########  MAIN STARTING POINT  #########
 if __name__ == '__main__':
+
+    # We create the thread for the city menu
+    thread=threading.Thread(target=changeCitySystem)
+    thread.daemon = True
+    thread.start()
+
+    # We begin running the API
+    try:
+        app.run()
+    except KeyboardInterrupt:
+        print(f"[APP HANDLER] APPLICATION ENDED DUE TO HUMAN INTERACTION")
+    except Exception as e:
+        print(f"[APP HANDLER] THERE HAS BEEN AN ERROR WHILE RUNNING THE APP. {e}")
     
-    CITYLock = threading.Lock()
-
-    # Realizamos una llamada inicial para establecer la ciudad
-    changeCitySystem()
-
-    # Hilo que se encarga de enviar OK o KO a los componentes que llamen la API de EC_CTC
-    threadgSendWeather = threading.Thread(target=apiWrapper)
-    # Búcle para cambiar la ciudad
-    while True:
-        changeCitySystem()
---
+++ EC_Registry.py
@@ -0,0 +1,137 @@
+from flask import Flask, request, jsonify
+import threading
+import tkinter as tk
+import sqlite3
+
+################ PATHS/CONSTANTS  ################
+
+Certificate = 'RegistryCertificate/cert.pem'
+Key = 'RegistryCertificate/key.pem'
+DATABASE = 'data/database.db'
+dbLock = threading.Lock()
+
+################  TABLE CREATIONS  ###############
+# DESCRIPTION: Este comprueba que exista la tabla Registry en la DB. Si no existe, la crea
+# STARTING_VALUES: NONE
+# RETURNS: NONE
+# NEEDS: NONE
+def checkRegistryTableDB():
+    global dbLock
+    try:
+        with dbLock:
+            # We first connect to the database
+            conn = sqlite3.connect(DATABASE)
+            # After that, we create the cursor to handle the database
+            c = conn.cursor()
+            # We check wheter the table registry is created or not
+            c.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='Registry'")
+            exists = c.fetchone()
+            # In case it exists, we print that the table already exists,
+            if exists:
+                print(f'[DATABASE HANDLER] Table Registry found')
+            # but in case it doesn't, we create the table
+            else:
+                print(f'[DATABASE HANDLER] Table Registry not found. Proceeding to table creation...')
+                c.execute("CREATE TABLE Registry (id text PRIMARY KEY, password text NOT NULL)")
+            # We commit the changes done to the database
+            conn.commit()
+            # Then we close the database
+            conn.close()
+    except Exception as e:
+        print(f'[DATABASE MANAGER] THERE HAS BEEN AN ERROR WHILE GETTING THE DATABASE TABLES. {e}')
+        conn.close()
+
+################  API DEFINITION  ################
+
+app = Flask(__name__)
+# DESCRIPTION: Este método dará de alta al taxi
+# STARTING_VALUES: NONE
+# RETURNS: NONE
+# NEEDS: NONE
+@app.route('/addTaxi', methods=['POST'])
+def addTaxi():
+    try:
+        global dbLock
+        # We first get the data payload from the request
+        data = request.json
+        # Then we extract the information from it
+        id = data.get('id')
+        password = data.get('password')
+        # Now we connect to the database
+        with dbLock:
+            conn = sqlite3.connect(DATABASE)
+            # We get the cursor to handle to DB connection
+            c = conn.cursor()
+            # And make sure to fetch all the entries of the DB
+            c.execute(f"SELECT * FROM Registry")
+            entries = [item for item in c.fetchall()]
+            flag = False
+            # Then we compare the id sent with every existing id, in case the taxi is already registered
+            for entry in entries:
+                # If it is, we set the flag to True
+                if entry[0] == id:
+                    flag = True
+            if not flag:
+                c.execute(f"INSERT INTO Registry VALUES ('{id}', '{password}')")
+                response = jsonify({'response': 'OK'})
+            else:
+                response = jsonify({'response': 'KO'})
+            # Then we commit the changes and close the connection
+            conn.commit()
+            conn.close()
+        return response
+    except Exception as e:
+        print(f"[TAXI DELETION] THERE HAS BEEN AN ERROR WHEN TRYING TO ADD A TAXI. {e}")
+        response = jsonify({'response': 'ERR'})
+        return response
+
+# DESCRIPTION: Este método dará de baja al taxi
+# STARTING_VALUES: NONE
+# RETURNS: NONE
+# NEEDS: NONE
+@app.route('/deleteTaxi', methods=['POST'])
+def deleteTaxi():
+    try:
+        global dbLock
+        # We first get the data payload from the request
+        data = request.json
+        # Then we extract the information from it
+        id = data.get('id')
+        password = data.get('password')
+        # Now we connect to the database
+        with dbLock:
+            conn = sqlite3.connect(DATABASE)
+            # We get the cursor to handle to DB connection
+            c = conn.cursor()
+            # And make sure to fetch all the entries of the DB
+            c.execute(f"SELECT * FROM Registry")
+            entries = [item for item in c.fetchall()]
+            flag = False
+            # Then we compare the id sent with every existing id, in case the taxi is already registered
+            for entry in entries:
+                # If it is, we set the flag to True
+                if entry[0] == id and entry[1] == password:
+                    flag = True
+            if flag:
+                c.execute(f"DELETE FROM Registry WHERE id = '{id}'")
+                response = jsonify({'response': 'OK'})
+            else:
+                response = jsonify({'response': 'KO'})
+            # Then we commit the changes and close the connection
+            conn.commit()
+            conn.close()
+        return response
+    except Exception as e:
+        print(f"[TAXI DELETION] THERE HAS BEEN AN ERROR WHEN TRYING TO DELETE A TAXI. {e}")
+        response = jsonify({'response': 'ERR'})
+        return response
+    
+#########  MAIN STARTING POINT  #########
+if __name__ == '__main__':
+    # We check for the DB
+    checkRegistryTableDB()
+    # We begin running the application
+    try:
+        app.run(port=5000, ssl_context=(Certificate, Key))
+    except Exception as e:
+        print(f'[APPLICATION HANDLER] THERE HAS BEEN AN ERROR WHILE HANDLING THE APPLICATION. {e}')
